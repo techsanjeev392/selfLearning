b50548d9a8dc225f0586b99fc45311ac4fa765ec
SYSTCS471 <sanjeev.3.kumar@morrisonsplc.co.uk>
2024-02-21 17:53:40 +0530

    Added new code

---
+++ example1/src/main/java/com/example1/main/Example1.java
@@ -0,0 +1,30 @@
+package com.example1.main;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.sanjeev.example1.beans.Vehicle;
+import com.sanjeev.example1.config.ProjectConfig;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Example1 {
+	
+	public static void main(String[] args) {
+		var context= new AnnotationConfigApplicationContext(ProjectConfig.class) ;
+		
+		var veh = context.getBean("vehicle1",Vehicle.class);
+		log.info("vehical Audi context name ::" + veh.getName());
+		
+		var veh2 = context.getBean("vehicle2",Vehicle.class);
+		log.info("vehical Honda context name ::" + veh2.getName());
+		
+		var str= context.getBean(String.class);
+		
+		log.info("String beal :: "+str);
+		var int1 =context.getBean(int.class);
+		log.info("Int value from the Bean :: "+ int1);
+		
+	}
+
+}
---
+++ example1/src/main/java/com/sanjeev/example1/config/ProjectConfig.java
@@ -0,0 +1,35 @@
+package com.sanjeev.example1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.sanjeev.example1.beans.Vehicle;
+
+@Configuration
+public class ProjectConfig {
+	
+	@Bean
+	Vehicle vehicle1() {
+		var veh = new Vehicle();
+		veh.setName("Audi 888");
+		return veh;
+	}
+	
+	@Bean
+	Vehicle vehicle2() {
+		var veh = new Vehicle();
+		veh.setName("Honda");
+		return veh;
+	}
+	
+	@Bean
+	String hello() {
+		return "Hello";
+	}
+	@Bean
+	int intgerValue() {
+		return 233;
+	}
+	
+
+}
